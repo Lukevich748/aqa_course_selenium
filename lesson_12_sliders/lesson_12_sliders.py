42efc4d39aace11c213905dd56f463d967530d79
Lukevich <luckevichartem@gmail.com>
2024-06-24 12:12:39 +0200

    sliders

--- lesson_12_sliders/lesson_12_sliders.py
+++ lesson_12_sliders/lesson_12_sliders.py
@@ -1,12 +1,8 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
 options = webdriver.ChromeOptions()
 options.add_argument("--window-size=1920,1080")
-
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://seiyria.com/bootstrap-slider/")
@@ -18,15 +14,8 @@ CURRENT_SLIDER_VALUE_LOCATOR = ("xpath", "//div[@id='example-6']//span[@id='ex6S
 driver.find_element(*EXAMPLE_6_LOCATOR).click()
 
 
-def move_slider(slider_locator: tuple, current_point_attr_name: str, target_point: int):
-    min_value = -5
-    max_value = 20
-
-    if target_point < min_value or target_point > max_value:
-        raise ValueError(f"Target point '{target_point}' is out of range. Must be between '{min_value}' and '{max_value}'.")
-
+def move_slider(slider_locator: tuple, current_point: int, target_point: int):
     slider = driver.find_element(*slider_locator)
-    current_point = int(slider.get_attribute(current_point_attr_name))
 
     if target_point < current_point:
         offset = current_point - target_point
@@ -35,12 +24,27 @@ def move_slider(slider_locator: tuple, current_point_attr_name: str, target_poin
         offset = target_point - current_point
         slider.send_keys(Keys.ARROW_RIGHT * offset)
 
+
+def set_slider_example_6(slider_locator: tuple, current_point_attr_name: str, target_point: int):
+    min_value = -5
+    max_value = 20
+
+    slider = driver.find_element(*slider_locator)
+
+    current_point = int(slider.get_attribute(current_point_attr_name))
+
+    move_slider(slider_locator, current_point, target_point)
+
+    if target_point < min_value or target_point > max_value:
+        raise ValueError(
+            f"Target point '{target_point}' is out of range. Must be between '{min_value}' and '{max_value}'.")
+
     current_slider_value = int(driver.find_element(*CURRENT_SLIDER_VALUE_LOCATOR).text)
     assert target_point == current_slider_value, "Target point is not correct"
 
 
-move_slider(slider_locator=SLIDER_LOCATOR,
-            current_point_attr_name="aria-valuenow",
-            target_point=10)
+set_slider_example_6(slider_locator=SLIDER_LOCATOR,
+                     current_point_attr_name="aria-valuenow",
+                     target_point=20)
 
 driver.quit()
